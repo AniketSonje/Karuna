4e69d69b379a6644ae52d0b983718b90fc8b3b43
Abhinav Pandey <abhinavpandeyri2@gmail.com>
2023-02-20 16:04:08 +0530

    Workspace Added

---
+++ KarunaWorkspace/KarunaApp/KarunaApp/src/main/java/com/app/KarunaApp/KarunaAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.KarunaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarunaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarunaAppApplication.class, args);
+	}
+
+}
